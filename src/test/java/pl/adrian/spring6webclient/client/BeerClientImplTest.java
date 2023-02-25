fde45441c6d962a84367b5bd8d24eb26abdcdb9a
Adiks <apacholak97@interia.pl>
2023-02-25 15:42:08 +0100

    get list as java map

--- src/test/java/pl/adrian/spring6webclient/client/BeerClientImplTest.java
+++ src/test/java/pl/adrian/spring6webclient/client/BeerClientImplTest.java
@@ -1,10 +1,13 @@
 package pl.adrian.spring6webclient.client;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.awaitility.Awaitility.await;
 
 @SpringBootTest
 class BeerClientImplTest {
@@ -12,9 +15,32 @@ class BeerClientImplTest {
     @Autowired
     BeerClient beerClient;
 
+    AtomicBoolean atomicBoolean;
+
+    @BeforeEach
+    void setUp() {
+        atomicBoolean = new AtomicBoolean(false);
+    }
+
     @Test
     void listBeer() {
         beerClient.listBeer()
-                .subscribe(System.out::println);
+                .subscribe(response -> {
+                    System.out.println(response);
+                    atomicBoolean.set(true);
+                });
+
+        await().untilTrue(atomicBoolean);
+    }
+
+    @Test
+    void listBeersMap() {
+        beerClient.listBeersMap()
+                .subscribe(response -> {
+                    System.out.println(response);
+                    atomicBoolean.set(true);
+                });
+
+        await().untilTrue(atomicBoolean);
     }
 }
