eef10d97c39e64e4a8bc30c42db62139d1d704fd
Adiks <apacholak97@interia.pl>
2023-02-26 12:29:25 +0100

    webclient filter configuration

--- src/main/java/pl/adrian/spring6webclient/config/WebClientConfig.java
+++ src/main/java/pl/adrian/spring6webclient/config/WebClientConfig.java
@@ -3,6 +3,8 @@ package pl.adrian.spring6webclient.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -10,12 +12,23 @@ public class WebClientConfig implements WebClientCustomizer {
 
     private final String ROOT_URL;
 
-    public WebClientConfig(@Value("${webclient.root-url}") String rootUrl) {
+    private final ReactiveOAuth2AuthorizedClientManager authorizedClientManager;
+
+    public WebClientConfig(@Value("${webclient.root-url}") String rootUrl,
+                           ReactiveOAuth2AuthorizedClientManager authorizedClientManager) {
         this.ROOT_URL = rootUrl;
+        this.authorizedClientManager = authorizedClientManager;
     }
 
     @Override
     public void customize(WebClient.Builder webClientBuilder) {
-        webClientBuilder.baseUrl(ROOT_URL);
+        ServerOAuth2AuthorizedClientExchangeFilterFunction oauthFilterFunction =
+                new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+
+        oauthFilterFunction.setDefaultClientRegistrationId("springauth");
+
+        webClientBuilder
+                .filter(oauthFilterFunction)
+                .baseUrl(ROOT_URL);
     }
 }
